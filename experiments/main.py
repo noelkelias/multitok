10c186ac9b144e52dc60aaa8c762eb688bb90397
Noel Elias <30335119+noelkelias@users.noreply.github.com>
2025-10-11 13:57:33 -0700

    Update main.py

--- experiments/main.py
+++ experiments/main.py
@@ -16,8 +16,8 @@ from sklearn import metrics
 from multitok import *
 from bert import *
 from bert_multitok import *
-from gpt2 import *
-from gpt2_multitok import *
+from gpt import *
+from gpt_multitok import *
 from multitok_freq import *
 from random import *
 from model import *
@@ -52,13 +52,13 @@ train_eval(X1, Y, loader, test_X1, test_Y, vocab_size, 1, 20)
 X1, Y, loader, test_X1, test_Y, vocab_size =  bert_tokens(train_sentences, train_labels, test_sentences, test_labels)
 train_eval(X1, Y, loader, test_X1, test_Y, vocab_size, 1, 20)
 
-X1, Y, loader, test_X1, test_Y, vocab_size =  gpt2_tokens(train_sentences, train_labels, test_sentences, test_labels)
+X1, Y, loader, test_X1, test_Y, vocab_size =  gpt_tokens(train_sentences, train_labels, test_sentences, test_labels)
 train_eval(X1, Y, loader, test_X1, test_Y, vocab_size, 1, 20)
 
 X1, Y, loader, test_X1, test_Y, vocab_size =  multitok_tokens(train_sentences, train_labels, test_sentences, test_labels, 2, 1)
 train_eval(X1, Y, loader, test_X1, test_Y, vocab_size, 2, 20)
 
-X1, Y, loader, test_X1, test_Y, vocab_size =  gpt2_multitok_tokens(train_sentences, train_labels, test_sentences, test_labels, 2, 1)
+X1, Y, loader, test_X1, test_Y, vocab_size =  gpt_multitok_tokens(train_sentences, train_labels, test_sentences, test_labels, 2, 1)
 train_eval(X1, Y, loader, test_X1, test_Y, vocab_size, 1, 20)
 
 X1, Y, loader, test_X1, test_Y, vocab_size =  multitok_freq_tokens(train_sentences, train_labels, test_sentences, test_labels, 2, 1)
